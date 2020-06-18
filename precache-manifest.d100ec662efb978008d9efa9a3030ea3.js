c10ca9533a0ae5ee547218000f37ab43e1340433
alwisofwan <alwi>
2020-06-18 15:06:20 +0700

    Updates

---
+++ precache-manifest.d100ec662efb978008d9efa9a3030ea3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef54509bd3d694dbe2c7a90958c3ea9",
+    "url": "/TodoApps/index.html"
+  },
+  {
+    "revision": "dcee7951831b5cb32d07",
+    "url": "/TodoApps/static/css/main.b09a639c.chunk.css"
+  },
+  {
+    "revision": "3b8c19922b738d9d5ed5",
+    "url": "/TodoApps/static/js/2.76cea6cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoApps/static/js/2.76cea6cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcee7951831b5cb32d07",
+    "url": "/TodoApps/static/js/main.b7de239e.chunk.js"
+  },
+  {
+    "revision": "59b10a12b56e38713949",
+    "url": "/TodoApps/static/js/runtime-main.2d7f01aa.js"
+  },
+  {
+    "revision": "2f578f6d08919aa98ae622f1e14de423",
+    "url": "/TodoApps/static/media/bg.2f578f6d.png"
+  }
+]);
